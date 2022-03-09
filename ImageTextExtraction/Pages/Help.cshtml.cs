27cb5d5ec0534e04b248d93deff0e4e800f1d982
Oisin Keeney <L00108790@student.lyit.ie>
2022-03-09 16:25:48 +0000

    Project Skeleton

---
+++ ImageTextExtraction/Pages/Help.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImageTextExtraction.Pages;
+
+public class HelpModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ ImageTextExtraction/TextractClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using Amazon;
+using Amazon.Textract;
+using Amazon.Textract.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
+public class TextractClient
+{
+	
+}
