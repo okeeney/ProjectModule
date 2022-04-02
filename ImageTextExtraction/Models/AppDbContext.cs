eb75ecf7d70e8a76aaf1cc26aa1809ae5a10a57a
Oisin Keeney <L00108790@student.lyit.ie>
2022-04-02 15:47:59 +0100

    Further modifications to add db support

--- ImageTextExtraction/Models/AppDbContext.cs
+++ ImageTextExtraction/Models/AppDbContext.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+
 namespace ImageTextExtraction.Models
 {
     public class AppDbContext: DbContext 
     {
+        protected readonly IConfiguration Configuration;
+
         public AppDbContext() { }
 
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
 
         public virtual DbSet<UserDocument> Documents { get; set; }
     }
